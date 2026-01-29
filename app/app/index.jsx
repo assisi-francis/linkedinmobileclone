a6c8741a379f90ebb93db32fd43bb75c0af4099a
assisi-francis <doncyco123@gmail.com>
2026-01-29 01:36:44 +0100

    add posts

--- app/app/index.jsx
+++ app/app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View, Image, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity, ScrollView } from "react-native";
 import { SafeAreaFrameContext, SafeAreaView } from "react-native-safe-area-context";
 import Navbar from "../components/navbar";
 import Horizontal from "../components/horizontalline";
@@ -20,56 +20,266 @@ export default function Index()  {
       <Navbar filePath={require("../assets/profile.jpg")} />
       <Horizontal />
 
-      <ProfileHeader
-        profileImage={require("../assets/p1.jpeg")}
-        name="Don Francis"
-        title="Software Engineer at TechCorp"
-        website="www.donfrancis.dev"
-        timePosted="2h ago"
-        followConnect="Connect"
-      />
-
-      <ExpandableText
-          text="Excited to share that I've started a new position as a Software Engineer at TechCorp! Looking forward to this new chapter and the challenges ahead."
-          limit={75}
-          style={{ paddingHorizontal: 20, fontSize: 14, fontFamily: "Inter-Regular", paddingVertical: 0}}
-      />
-
-      <PostImage
-        source={require("../assets/p1.jpeg")}
-      />
-
-      <PostStats 
-        likes='300'
-        comments='50'
-        reposts='500'
-        icon={require("../assets/response.jpg")}
-      />
-
-      <View style={{ flexDirection: "row", justifyContent: "space-around", paddingVertical: 15, borderTopWidth: 1, borderTopColor: "#eee"   }}>
-        <ActionButton
-          icon={require("../assets/like.png")}
-          label="Like"
-          onPress={() => console.log("Like button pressed")}
+      <ScrollView showsVerticalScrollIndicator={false}>
+      
+        <ProfileHeader
+          profileImage={require("../assets/p1.jpeg")}
+          name="Don Francis"
+          title="Software Engineer at TechCorp"
+          website="www.donfrancis.dev"
+          timePosted="2h ago"
+          followConnect="Connect"
         />
-        <ActionButton
-          icon={require("../assets/comment.png")}
-          label="Comment"
-          onPress={() => console.log("Comment button pressed")}
-        />
-        <ActionButton
-          icon={require("../assets/repost.png")}
-          label="Repost"
-          onPress={() => console.log("Repost button pressed")}
-        />
-        <ActionButton
-          icon={require("../assets/send.png")}
-          label="Share"
-          onPress={() => console.log("Share button pressed")}
-        />
-      </View>
 
-      <Horizontal />
+        <ExpandableText
+            text="Excited to share that I've started a new position as a Software Engineer at TechCorp! Looking forward to this new chapter and the challenges ahead."
+            limit={75}
+            style={{ paddingHorizontal: 20, fontSize: 14, fontFamily: "Inter-Regular", paddingVertical: 0}}
+        />
+
+        <PostImage
+          source={require("../assets/p1.jpeg")}
+        />
+
+        <PostStats 
+          likes='100'
+          comments='150'
+          reposts='50'
+          icon={require("../assets/response.jpg")}
+        />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-around", paddingVertical: 15, borderTopWidth: 1, borderTopColor: "#eee"   }}>
+          <ActionButton
+            icon={require("../assets/like.png")}
+            label="Like"
+            onPress={() => console.log("Like button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/comment.png")}
+            label="Comment"
+            onPress={() => console.log("Comment button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/repost.png")}
+            label="Repost"
+            onPress={() => console.log("Repost button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/send.png")}
+            label="Share"
+            onPress={() => console.log("Share button pressed")}
+          />
+        </View>
+
+        <Horizontal />
+
+        <ProfileHeader
+          profileImage={require("../assets/p2.jpeg")}
+          name="John James"
+          title="Electrical Electronics Engineer at ElectroWorks"
+          website="www.electroworks.com"
+          timePosted="3h ago"
+          followConnect="Connect"
+        />
+
+        <ExpandableText
+            text="Excited to share that I've started a new position as a Software Engineer at TechCorp! Looking forward to this new chapter and the challenges ahead."
+            limit={75}
+            style={{ paddingHorizontal: 20, fontSize: 14, fontFamily: "Inter-Regular", paddingVertical: 0}}
+        />
+
+        <PostImage
+          source={require("../assets/post1.jpg")}
+        />
+
+        <PostStats 
+          likes='300'
+          comments='50'
+          reposts='200'
+          icon={require("../assets/response.jpg")}
+        />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-around", paddingVertical: 15, borderTopWidth: 1, borderTopColor: "#eee"   }}>
+          <ActionButton
+            icon={require("../assets/like.png")}
+            label="Like"
+            onPress={() => console.log("Like button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/comment.png")}
+            label="Comment"
+            onPress={() => console.log("Comment button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/repost.png")}
+            label="Repost"
+            onPress={() => console.log("Repost button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/send.png")}
+            label="Share"
+            onPress={() => console.log("Share button pressed")}
+          />
+        </View>
+
+        <Horizontal />
+
+        <ProfileHeader
+          profileImage={require("../assets/p3.jpeg")}
+          name="Daniel Blessing Smith"
+          title="Content Writer at WriteWell"
+          website="www.writewell.dev"
+          timePosted="4h ago"
+          followConnect="Follow"
+        />
+
+        <ExpandableText
+            text="Excited to share that I've started a new position as a Software Engineer at TechCorp! Looking forward to this new chapter and the challenges ahead."
+            limit={75}
+            style={{ paddingHorizontal: 20, fontSize: 14, fontFamily: "Inter-Regular", paddingVertical: 0}}
+        />
+
+        <PostImage
+          source={require("../assets/post2.jpg")}
+        />
+
+        <PostStats 
+          likes='370'
+          comments='110'
+          reposts='70'
+          icon={require("../assets/response.jpg")}
+        />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-around", paddingVertical: 15, borderTopWidth: 1, borderTopColor: "#eee"   }}>
+          <ActionButton
+            icon={require("../assets/like.png")}
+            label="Like"
+            onPress={() => console.log("Like button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/comment.png")}
+            label="Comment"
+            onPress={() => console.log("Comment button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/repost.png")}
+            label="Repost"
+            onPress={() => console.log("Repost button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/send.png")}
+            label="Share"
+            onPress={() => console.log("Share button pressed")}
+          />
+        </View>
+
+        <Horizontal />
+
+
+        <ProfileHeader
+          profileImage={require("../assets/p4.jpeg")}
+          name="Sunday Williams"
+          title="Estate Agent at HomeFinders"
+          website="www.homefinders.net"
+          timePosted="5h ago"
+          followConnect="Connect"
+        />
+
+        <ExpandableText
+            text="Excited to share that I've started a new position as a Software Engineer at TechCorp! Looking forward to this new chapter and the challenges ahead."
+            limit={75}
+            style={{ paddingHorizontal: 20, fontSize: 14, fontFamily: "Inter-Regular", paddingVertical: 0}}
+        />
+
+        <PostImage
+          source={require("../assets/gem1.png")}
+        />
+
+        <PostStats 
+          likes='400'
+          comments='90'
+          reposts='440'
+          icon={require("../assets/response.jpg")}
+        />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-around", paddingVertical: 15, borderTopWidth: 1, borderTopColor: "#eee"   }}>
+          <ActionButton
+            icon={require("../assets/like.png")}
+            label="Like"
+            onPress={() => console.log("Like button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/comment.png")}
+            label="Comment"
+            onPress={() => console.log("Comment button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/repost.png")}
+            label="Repost"
+            onPress={() => console.log("Repost button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/send.png")}
+            label="Share"
+            onPress={() => console.log("Share button pressed")}
+          />
+        </View>
+
+        <Horizontal />
+
+          <ProfileHeader
+          profileImage={require("../assets/p5.jpeg")}
+          name="Jimmy Khabby"
+          title="Digital Marketer at MarketPros"
+          website="www.marketpros.com"
+          timePosted="6h ago"
+          followConnect="Follow"
+        />
+
+        <ExpandableText
+            text="Excited to share that I've started a new position as a Software Engineer at TechCorp! Looking forward to this new chapter and the challenges ahead."
+            limit={75}
+            style={{ paddingHorizontal: 20, fontSize: 14, fontFamily: "Inter-Regular", paddingVertical: 0}}
+        />
+
+        <PostImage
+          source={require("../assets/gem2.png")}
+        />
+
+        <PostStats 
+          likes='10'
+          comments='80'
+          reposts='550'
+          icon={require("../assets/response.jpg")}
+        />
+
+        <View style={{ flexDirection: "row", justifyContent: "space-around", paddingVertical: 15, borderTopWidth: 1, borderTopColor: "#eee"   }}>
+          <ActionButton
+            icon={require("../assets/like.png")}
+            label="Like"
+            onPress={() => console.log("Like button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/comment.png")}
+            label="Comment"
+            onPress={() => console.log("Comment button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/repost.png")}
+            label="Repost"
+            onPress={() => console.log("Repost button pressed")}
+          />
+          <ActionButton
+            icon={require("../assets/send.png")}
+            label="Share"
+            onPress={() => console.log("Share button pressed")}
+          />
+        </View>
+
+        <Horizontal />
+
+
+      </ScrollView>
 
       <View style={{ flexDirection: "row", alignItems: "center", padding: 15, justifyContent: "space-around" }}>
         <ActionButton
@@ -102,8 +312,6 @@ export default function Index()  {
           onPress={() => console.log("notifications button pressed")}
           iconSize={25}
         />  
-
-
       </View>
 
 
--- app/components/ActionButton.jsx
+++ app/components/ActionButton.jsx
@@ -1,11 +1,11 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 
-const ActionButton = ({ icon, label, onPress, iconSize = 18, textStyle }) => {
+const ActionButton = ({ icon, label, onPress, style, textStyle }) => {
   return (
     <TouchableOpacity onPress={onPress} style={[{ alignItems: "center" }]}>
       <Image
         source={icon}
-        style={{ width: iconSize, height: iconSize, resizeMode: "contain" }}
+        style={{ width: 18, height: 18, resizeMode: "contain" }}
       />
       <Text style={[{ fontSize: 12, color: "black", marginTop: 4 }, textStyle]}>
         {label}
